92ac91e9c1dd86037406a58d04cff13872a470dc
Md Atiq <shahriyarislam02@gmail.com>
2025-09-10 22:22:47 +0100

    Added a function to update our results. When user stops it  will print the results showing how many times the colours were randomly choosen. Also added emoji to make user easier to read :) .

--- 01_wheel_of_colours/README.md
+++ 01_wheel_of_colours/README.md
@@ -6,7 +6,14 @@ DIFFICULTY - 2/4
 Version Notes
 
 Version 1.0:
+
     Very basic interactive colour picker for user. It selects random colour from a list for the user.
+
     What I learnt:
     Had to recall how to print all the list using '.join()' method and random number generator.
     It will now help me remember these better than tutorials.
+
+Version 2.0:
+
+    Added a counter to count the number of spins done and dictionary 'results' that store the colour(as key)
+    and number of times(as a value) they were chosen
--- 01_wheel_of_colours/WheelOfColours.py
+++ 01_wheel_of_colours/WheelOfColours.py
@@ -5,22 +5,35 @@ num_colours = len(colours)
 spin_counter = 0
 results = {}
 
+def update_results(colour):
+    # check if the colour exists in the dictionary
+    if colour not in results:
+        results[colour] = 1
+        return
+    
+    results[colour] = results.get(colour) + 1
+
 print("\nHello! Welcome to Wheel of Colours.")
 #tells the user of the default colours on the wheel
 print(f"Current colours on the wheel are: {', '.join(colours)} \n")
 print("To spin the wheel type 'spin'")
-print("And to stop type 'stop'")
+print("And to stop type 'stop'\n")
 
 user_command = input("spin/stop:  ")
 
 while user_command != "stop":
     # '-1' to prevent it from getting index out of range
     # as indexing in list starts from 0 i.e. first value
-    random_colour = random.randint(0, num_colours - 1)
+    random_colour_index = random.randint(0, num_colours - 1)
 
     spin_counter += 1
+    update_results(colours[random_colour_index])
 
-    print(colours[random_colour])
+    print(f"🎉 {colours[random_colour_index]} 🎉")
+    print(f"\nNumber of spins = {spin_counter}\n")
     user_command = input("spin/stop:  ")
 
+print("\n Results: \n")
+for key, value in results.items():
+    print(f"{key} : {value}")
 print("\nThank you using Wheel of Colours!")
