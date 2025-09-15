25fe094f23e988b7faead2dffd6d6fbdeb07f7ef
Md Atiq <shahriyarislam02@gmail.com>
2025-09-15 23:35:48 +0100

    Added maximumnumber of attempts.

--- 02_number_guessing_game/GuessTheNumber.py
+++ 02_number_guessing_game/GuessTheNumber.py
@@ -8,21 +8,28 @@ def guess_the_number():
     max_value = int(input("Maximum value:  "))
 
     rand_number = random.randint(min_value, max_value)
-    user_attempts = 0
+    user_attempts = 1
+    max_attempts = 5
     
-    user_guess = int(input("Guess the number (between 1 and 100):  "))
+    print(f"You have {max_attempts} attempts")
+    user_guess = int(input(f"Guess the number (between {min_value} and {max_value}):  "))
 
-    while user_guess != rand_number:
-        user_attempts += 1
-
+    while (user_guess != rand_number) and user_attempts != max_attempts:
+        
         if user_guess < rand_number:
-            print("Too Low! Try again.")
+            print(f"Too Low! Try again.")
+            print(f"{max_attempts - user_attempts} attempts left.\n")
         else:
             print("Too High! Try again.")
+            print(f"{max_attempts - user_attempts} attempts left.\n")
+        
+        user_guess = int(input(f"Guess the number (between {min_value} and {max_value}):  "))
+        user_attempts += 1
 
-        user_guess = int(input("Guess the number (between 1 and 100):  "))
-
-    print(f"\nCongratulations! You guessed the number in {user_attempts} attempts.\n")
+    if user_attempts == max_attempts:
+        print("Game Over!. You ran out of attempts.")
+    else:
+        print(f"\nCongratulations! You guessed the number in {user_attempts} attempts.\n")
 
 #RUN
 guess_the_number()
