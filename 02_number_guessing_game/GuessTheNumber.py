ffc237f435950281e791988c4eab194dcbf39bac
Md Atiq <shahriyarislam02@gmail.com>
2025-09-17 23:51:40 +0100

    Refactored and cleaned

--- 02_number_guessing_game/GuessTheNumber.py
+++ 02_number_guessing_game/GuessTheNumber.py
@@ -8,28 +8,35 @@ def guess_the_number():
     max_value = int(input("Maximum value:  "))
 
     rand_number = random.randint(min_value, max_value)
-    user_attempts = 1
+    user_attempts = 0
     max_attempts = 5
     
     print(f"You have {max_attempts} attempts")
-    user_guess = int(input(f"Guess the number (between {min_value} and {max_value}):  "))
 
-    while (user_guess != rand_number) and user_attempts != max_attempts:
-        
-        if user_guess < rand_number:
-            print(f"Too Low! Try again.")
-            print(f"{max_attempts - user_attempts} attempts left.\n")
-        else:
-            print("Too High! Try again.")
-            print(f"{max_attempts - user_attempts} attempts left.\n")
-        
-        user_guess = int(input(f"Guess the number (between {min_value} and {max_value}):  "))
+    while user_attempts != max_attempts:
+        try:
+            user_guess = int(input(f"Guess the number (between {min_value} and {max_value}):  "))
+        except ValueError:
+            print("❌ Invalid input. Please Enter a valid number.")
+            continue
+
         user_attempts += 1
 
-    if user_attempts == max_attempts:
-        print("Game Over!. You ran out of attempts.")
-    else:
-        print(f"\nCongratulations! You guessed the number in {user_attempts} attempts.\n")
+        if user_guess == rand_number:
+            print(f"\nCongratulations! You guessed the number in {user_attempts} attempts.\n")
+            break
+
+        if user_guess < rand_number:
+            print(f"Too Low! Try again.")
+        else:
+            print("Too High! Try again.")
+        
+        if user_attempts == max_attempts:
+            print("Game Over!. You ran out of attempts.")
+            break
+
+        print(f"{max_attempts - user_attempts} attempts left.\n")
+
 
 #RUN
 guess_the_number()
--- 02_number_guessing_game/README.md
+++ 02_number_guessing_game/README.md
@@ -5,7 +5,7 @@ DIFFICULTY - 3/4
 ## Version Notes
 
 ### Version 1.0
-    Random number is selected using `randint()` method. User has to keep on guessing
+    Random number is selected using __randint()__ method. User has to keep on guessing
     until it is correct. On each guess user is informed if the number entered is 
     'Too High!' or 'Too Low!' and the number of attempts is counted.
 
@@ -13,3 +13,10 @@ DIFFICULTY - 3/4
     I allowed the user to specify the minimum and maximum values for the number
     range before the game starts. This gives the player more control over the
     difficulty level. 
+
+### Version 1.2
+    __try except__ is added to handle invalid input. Code in the last version was not 
+    clean and had some repeated lines. It had an issue with not stopping when 
+    **user_attempts == max_attempts** and so it was set to 1 at start just for it to work
+    properly. 
+    **It is now cleaner and more readable**
