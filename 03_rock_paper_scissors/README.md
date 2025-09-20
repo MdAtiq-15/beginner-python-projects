0ea977e06b0d2a25fcc30328a9f630fb721ecbd2
Md Atiq <shahriyarislam02@gmail.com>
2025-09-20 22:16:08 +0100

    Added a round, best of 3, and winner is printed.

---
+++ 03_rock_paper_scissors/README.md
@@ -0,0 +1,25 @@
+Program to simulate a game of Rock, Paper, Scissors.
+The game will prompt the player to choose rock, paper, or scissors by typing 'r',
+'p', or 's'. The computer will randomly select its choice. The game will then display
+both choices using emojis and determine the winner based on the rules.
+
+DIFFICULTY - 3/4
+
+## Version Notes
+
+### Version 1.0
+    The game will prompt the player to choose rock, paper, or scissors by typing 'r',
+    'p', or 's'. The computer will randomly select its choice. The game will then display
+    both choices using emojis and determine the winner based on the rules. 
+
+### Version 1.1
+    Added a feature if player want to continue or or not.
+    Modified the game so that the first player (or computer) to win two out of three
+    rounds is declared the overall winner. This adds a competitive aspect to the
+    game. 
+
+### Version 1.2
+    
+
+### Version 1.2
+    
--- 03_rock_paper_scissors/RockPaperScissors.py
+++ 03_rock_paper_scissors/RockPaperScissors.py
@@ -6,18 +6,50 @@ moves = {
     "s": "✂️"
     }
 
-while True:
+round_counter = 1
+player_round_wins = 0
+computer_round_wins = 0
+
+while round_counter <= 3:
+    print(f"ROUND {round_counter}")
     user = input("Rock, Paper or Scissors? (r/p/s): ").strip().lower()
     computer = random.choice(list(moves.keys()))
 
-    print(f"You chose {moves[user]}")
+    print(f"\nYou chose {moves[user]}")
     print(f"Computer chose {moves[computer]}")
 
     if user == computer:
         print("It's a tie!")
 
     elif (user == "r" and computer == "s") or (user == "p" and computer == "r") or (user == "s" and computer == "p"):
-        print("You win!")
+        print("You win!\n")
+        player_round_wins += 1
 
     else:
-        print("You lose!")
+        print("You lose!\n")
+        computer_round_wins += 1
+
+    # Check winner
+    if player_round_wins == 2:
+        print("Player wins the game!")
+        break
+    elif computer_round_wins == 2:
+        print("Computer wins the game!")
+        break
+    elif round_counter == 3:  # Only reaches here if it's 1-1 going into last round
+        if player_round_wins > computer_round_wins:
+            print("Player wins the game!")
+        elif computer_round_wins > player_round_wins:
+            print("Computer wins the game!")
+        else:
+            print("Round tied!")
+
+
+
+    # play_again = input("Continue? (y/n): ")
+    print("+", "-" * 30, "+")
+    # if play_again != "y":
+    #     break
+
+    round_counter += 1
+    
